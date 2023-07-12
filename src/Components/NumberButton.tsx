65fbd7727e17d09433afb87d2a3d707780b1273b
Dan Snow <danjsnow@outlook.com>
2023-07-12 11:33:32 +0100

    Adds horizontal option for number buttons
    Added via a flag that defaults to false - I already set it to
    true for the demand buttons but the unit price ones remain
    vertical.
    
    This saves space in the case of the demand icons - there still
    needs to be formatting changes e.g. the fact that the buttons
    demand buttons aren't quite aligned due to differently sized svgs
    but that's for a different commit.

--- src/Components/NumberButton.tsx
+++ src/Components/NumberButton.tsx
@@ -4,20 +4,31 @@ type NumberButtonProps = {
     label: string;
     onIncrement: () => void;
     onDecrement: () => void;
+    horizontal?: boolean;
   };
   
   const NumberButton: React.FC<NumberButtonProps> = ({
     label,
     onIncrement,
     onDecrement,
+    horizontal = false,
   }) => {
-    return (
+    const verticalCounter = (
       <div className='numberCruncher'>
         <button onClick={onIncrement}>▲</button>
         <span>{label}</span>
         <button onClick={onDecrement}>▼</button>
       </div>
     );
+    const horizontalCounter = (
+      <div className='numberCruncher' style={{display: 'flex', 
+      flexDirection: 'row', justifyContent: 'space-between'}}>
+        <button onClick={onDecrement}>◀</button>
+        <span>{label}</span>
+        <button onClick={onIncrement}>▶</button>
+      </div>
+    );
+    return horizontal ? horizontalCounter : verticalCounter;
   };
   
 
